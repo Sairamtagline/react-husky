600883b39a7bbbe5f9d269d60b07b8a712eb7f5e
Sairam Tagline <sairam.tagline@gmail.com>
2021-02-17 12:56:17 +0530

    sample commit

--- src/Sample.js
+++ src/Sample.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react'
 
 const Sample = () => {
     return (
-        <div>
-            Sample
-        </div>
+         <div>
+            Sample 
+         </div>
     )
 }
 
